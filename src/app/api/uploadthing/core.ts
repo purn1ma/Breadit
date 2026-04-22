e3051a703df3a6256d63a1632d5909f6b5a2d20c
Purnima <purnimashrivastava97@gmail>
2026-04-23 00:37:05 +0530

    fix: upgrade uploadthing to v7, fix image upload broken in v6

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,6 @@
 import { getAuthSession } from '@/lib/auth'
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
+import { UploadThingError } from 'uploadthing/server'
 
 const f = createUploadthing()
 
@@ -8,7 +9,7 @@ export const ourFileRouter = {
     .middleware(async () => {
       const session = await getAuthSession()
 
-      if (!session) throw new Error('Unauthorized')
+      if (!session) throw new UploadThingError('Unauthorized')
 
       return { userId: session.user.id }
     })
